045adbf9ed5a4bf015bd255f2fc665bc2d4262a2
Hogar <Hogar@DESKTOP-TMFGHLK>
2024-12-22 22:05:25 -0600

    Agregar archivos de proyecto.

---
+++ Pry_Prueba/Prueba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pry_Prueba
+{
+    internal class Prueba
+    {
+        public class Mesa
+        {
+
+            public Mesa() { }
+            public Mesa(int id) { } 
+    }
+}
