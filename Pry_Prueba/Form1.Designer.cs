b3cc2400e5b1a104012eac923f33f1684f6e0e4b
Julio Adrian Valdez Lopez <julio.valdez0019@gmail.com>
2024-12-22 23:03:35 -0600

    2da prueba

--- Pry_Prueba/Form1.Designer.cs
+++ Pry_Prueba/Form1.Designer.cs
@@ -31,12 +31,16 @@
             button1 = new Button();
             comboBox1 = new ComboBox();
             dataGridView1 = new DataGridView();
+            checkBox1 = new CheckBox();
+            button2 = new Button();
+            comboBox2 = new ComboBox();
+            groupBox1 = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(101, 78);
+            button1.Location = new Point(73, 49);
             button1.Name = "button1";
             button1.Size = new Size(308, 170);
             button1.TabIndex = 0;
@@ -58,12 +62,53 @@
             dataGridView1.Name = "dataGridView1";
             dataGridView1.Size = new Size(287, 150);
             dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(452, 42);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(424, 90);
+            button2.Name = "button2";
+            button2.Size = new Size(60, 36);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(414, 179);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(136, 319);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(141, 76);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(657, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(button2);
+            Controls.Add(checkBox1);
             Controls.Add(dataGridView1);
             Controls.Add(comboBox1);
             Controls.Add(button1);
@@ -71,6 +116,7 @@
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -78,5 +124,9 @@
         private Button button1;
         private ComboBox comboBox1;
         private DataGridView dataGridView1;
+        private CheckBox checkBox1;
+        private Button button2;
+        private ComboBox comboBox2;
+        private GroupBox groupBox1;
     }
 }
